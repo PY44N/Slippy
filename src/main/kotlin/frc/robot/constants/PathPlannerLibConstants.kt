008e034d04a3dd557bfdcc83d095d56ee3102c00
Dezash1 <desmondmehta@gmail.com>
2024-01-20 14:48:16 -0500

    probably works

---
+++ src/main/kotlin/frc/robot/constants/PathPlannerLibConstants.kt
@@ -0,0 +1,19 @@
+package frc.robot.constants
+
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig
+import com.pathplanner.lib.util.PIDConstants
+import com.pathplanner.lib.util.ReplanningConfig
+import frc.robot.constants.DriveConstants
+
+object PathPlannerLibConstants {
+    var replanningConfig = ReplanningConfig()
+    var translationPID = PIDConstants(0.0, 0.0)
+    var rotationPID = PIDConstants(0.0, 0.0)
+    var pathPlannerConfig = HolonomicPathFollowerConfig(
+        translationPID,
+        rotationPID,
+        DriveConstants.maxSpeed,
+        DriveConstants.robotRadius,
+        replanningConfig,
+    )
+}
