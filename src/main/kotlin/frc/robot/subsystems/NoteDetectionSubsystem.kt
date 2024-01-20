af23e907335283d6399ca034009a22daff71d89a
cassiopeia3 <warrenw3g@gmail.com>
2024-01-20 17:42:53 -0500

    Create NoteDetectionSubsystem.kt

---
+++ src/main/kotlin/frc/robot/subsystems/NoteDetectionSubsystem.kt
@@ -0,0 +1,9 @@
+package frc.robot.subsystems
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+
+class NoteDetectionSubsystem : SubsystemBase() {
+    fun findPos(): Array<Double>{
+        
+    }
+}
