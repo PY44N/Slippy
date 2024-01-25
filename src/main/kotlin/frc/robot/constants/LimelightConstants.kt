320ac8c47f7de9741ea0a49c6dfa8e03ab2a733e
s-s25 <stephenschmidt025@gmail.com>
2024-01-24 20:56:02 -0500

    .

---
+++ src/main/kotlin/frc/robot/constants/LimelightConstants.kt
@@ -0,0 +1,9 @@
+package frc.robot.constants
+
+object LimelightConstants {
+
+    const val limelightName1 : String = "limelight1"
+    const val limelightName2 : String = "limelight2"
+    const val limelightName3 : String = "limelight3"
+
+}
--- src/main/kotlin/frc/robot/subsystems/NoteDetectionSubsystem.kt
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase
-
-class NoteDetectionSubsystem : SubsystemBase() {
-    fun findPos(): Array<Double>{
-        
-    }
-}
