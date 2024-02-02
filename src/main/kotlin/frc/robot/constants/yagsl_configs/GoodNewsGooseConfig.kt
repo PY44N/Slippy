1416a69497861c2a0c227e7be703cf49085d570a
Dezash1 <desmondmehta@gmail.com>
2024-02-01 19:28:44 -0500

    fixed idsd

--- src/main/kotlin/frc/robot/constants/yagsl_configs/GoodNewsGooseConfig.kt
+++ src/main/kotlin/frc/robot/constants/yagsl_configs/GoodNewsGooseConfig.kt
@@ -20,19 +20,19 @@ object GoodNewsGooseConfig {
     val TWIST_PID = PIDFConfig(0.004, 1.5)
 
     val FRONT_LEFT_DRIVE_MOTOR = TalonFXSwerve(5, true)
-    val FRONT_LEFT_TWIST_MOTOR = SparkMaxSwerve(10, false)
+    val FRONT_LEFT_TWIST_MOTOR = SparkMaxSwerve(9, false)
     val FRONT_LEFT_ENCODER = CANCoderSwerve(13, "")
 
     val FRONT_RIGHT_DRIVE_MOTOR = TalonFXSwerve(2, true)
-    val FRONT_RIGHT_TWIST_MOTOR = SparkMaxSwerve(12, false)
+    val FRONT_RIGHT_TWIST_MOTOR = SparkMaxSwerve(6, false)
     val FRONT_RIGHT_ENCODER = CANCoderSwerve(10, "")
 
     val BACK_RIGHT_DRIVE_MOTOR = SparkMaxSwerve(3, true)
-    val BACK_RIGHT_TWIST_MOTOR = SparkMaxSwerve(11, false)
+    val BACK_RIGHT_TWIST_MOTOR = SparkMaxSwerve(7, false)
     val BACK_RIGHT_ENCODER = CANCoderSwerve(11, "")
 
     val BACK_LEFT_DRIVE_MOTOR = SparkMaxSwerve(4, true)
-    val BACK_LEFT_TWIST_MOTOR = SparkMaxSwerve(9, false)
+    val BACK_LEFT_TWIST_MOTOR = SparkMaxSwerve(8, false)
     val BACK_LEFT_ENCODER = CANCoderSwerve(12, "")
 
     val goodNewsGoose = YAGSLConfig(
