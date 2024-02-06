e3f759ad9b00f26451b2a6d64ba18c8e8d44fd53
Milan Lustig <mguinangreen726@gmail.com>
2024-02-05 20:39:10 -0500

    removed caliko

--- src/main/kotlin/frc/robot/subsystems/TrunkSystem.kt
+++ src/main/kotlin/frc/robot/subsystems/TrunkSystem.kt
@@ -2,30 +2,11 @@ package frc.robot.subsystems
 
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import au.edu.federation.caliko.FabrikStructure2D
-import au.edu.federation.caliko.FabrikChain2D
-import au.edu.federation.caliko.FabrikBone2D
-
 
 
 class TrunkSystem : SubsystemBase() {
 
     init {
-        val structure: FabrikStructure2D = FabrikStructure2D()
-        val chain: FabrikChain2D = FabrikChain2D()
-
-        val baseBone: FabrikBone2D = FabrikBone2D(0.0f, 0.0f, 0.0f, 0.0f)
-        baseBone.setJoint()
-
-
-        chain.addBone(baseBone)
-
-        val endBone: FabrikBone2D = FabrikBone2D(0.0f, 0.0f, 0.0f, 0.0f)
-        chain.addBone(endBone)
-
-
-
-        structure.addChain(chain)
     }
 
     fun setDesiredPosition(angle: Rotation2d, distance: Double) {}
