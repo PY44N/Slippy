23fe8a0affd0b5ac9e11e681130b2958c28294bd
Dezash1 <desmondmehta@gmail.com>
2024-02-09 22:42:10 -0500

    i will kill the president of the united states at 3:28 pm on June 10, 2024

--- src/main/kotlin/frc/robot/subsystems/GUNSystem.kt
+++ src/main/kotlin/frc/robot/subsystems/GUNSystem.kt
@@ -2,13 +2,21 @@ package frc.robot.subsystems
 
 import com.revrobotics.CANSparkLowLevel
 import com.revrobotics.CANSparkMax
-import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 class GUNSystem() : SubsystemBase() {
 
-    private val sparkMaxElevator = CANSparkMax(11, CANSparkLowLevel.MotorType.kBrushless)
+    private val elevatorMotor = CANSparkMax(11, CANSparkLowLevel.MotorType.kBrushless)
+    private val leftPivotMotor = CANSparkMax(13, CANSparkLowLevel.MotorType.kBrushless)
+    private val rightPivotMotor = CANSparkMax(14, CANSparkLowLevel.MotorType.kBrushless)
+//    private var currentPosition: Double
 
+    init {
+
+    }
+    fun setZeroPosition() {
+
+    }
     fun goToPosition(angle: Double, elevatorHeight: Double) {
 
     }
@@ -22,7 +30,7 @@ class GUNSystem() : SubsystemBase() {
     }
 
     fun elevate(speed : Double) {
-        sparkMaxElevator.set(speed)
+        elevatorMotor.set(speed)
     }
 
     override fun periodic() {
@@ -32,7 +40,15 @@ class GUNSystem() : SubsystemBase() {
     }
 
     companion object {
-        const val MAX_HEIGHT_M = 1.0;
-        const val MIN_HEIGHT_M = .5;
+        const val MAX_PIVOT_HEIGHT_M = 0.64446
+        const val MIN_PIVOT_HEIGHT_M = 0.348701
+        const val THING_LENGTH_M = 0.6133
     }
+}
+
+enum class GUNPosition {
+    AMP,
+    SPEAKER,
+    INTAKE,
+    MOVIN,
 }
