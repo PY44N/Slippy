ffd97e6dee06810e8040c6307df8b8cd3158640c
Dezash1 <desmondmehta@gmail.com>
2024-02-12 22:49:42 -0500

    removed python

--- src/main/python/InverseKinematics.py
+++
@@ -1,8 +0,0 @@
-from klampt import IKObjective, IKSolver, WorldModel, vis
-from klampt.model import ik
-
-world = WorldModel()
-world.loadElement("models/robot-urdf/robot.urdf")
-
-vis.add("world", world)
-vis.run()
