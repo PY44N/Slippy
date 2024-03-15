a39df2483ed9413759cfba394cc2f669e12c7ac1
Ryan <62762243+PY44N@users.noreply.github.com>
2024-03-15 16:22:50 -0400

    Chris says wires no route, so no code

--- src/main/kotlin/frc/robot/subsystems/cannon/CannonIOReal.kt
+++ src/main/kotlin/frc/robot/subsystems/cannon/CannonIOReal.kt
@@ -4,27 +4,25 @@ package frc.robot.subsystems.cannon
 import com.revrobotics.CANSparkLowLevel
 import com.revrobotics.CANSparkMax
 import edu.wpi.first.wpilibj.DigitalInput
-import frc.robot.RobotContainer
 import frc.robot.constants.CannonConstants
-import frc.robot.util.Telemetry
 
 class CannonIOReal : CannonIO {
 
     val leftShooterMotor: CANSparkMax =
-        CANSparkMax(CannonConstants.LEFT_SHOOTER_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless)
+            CANSparkMax(CannonConstants.LEFT_SHOOTER_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless)
     val rightShooterMotor: CANSparkMax =
-        CANSparkMax(CannonConstants.RIGHT_SHOOTER_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless)
+            CANSparkMax(CannonConstants.RIGHT_SHOOTER_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless)
 
 //    val leftShooterEncoder = leftShooterMotor.getAlternateEncoder(8192);
 //    val rightShooterEncoder = rightShooterMotor.getAlternateEncoder(8192);
 
-    val leftShooterEncoder = leftShooterMotor.getEncoder();
-    val rightShooterEncoder = rightShooterMotor.getEncoder();
+    val leftShooterMotorEncoder = leftShooterMotor.getEncoder();
+    val rightShooterMotorEncoder = rightShooterMotor.getEncoder();
 
     val outerIntakeMotor: CANSparkMax =
-        CANSparkMax(CannonConstants.OUTER_INTAKE_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless)
+            CANSparkMax(CannonConstants.OUTER_INTAKE_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless)
     val innerIntakeMotor: CANSparkMax =
-        CANSparkMax(CannonConstants.INNER_INTAKE_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless)
+            CANSparkMax(CannonConstants.INNER_INTAKE_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless)
 
     val entryBeamBreak: DigitalInput = DigitalInput(3)
     val loadedBeamBreak: DigitalInput = DigitalInput(2)
@@ -57,11 +55,11 @@ class CannonIOReal : CannonIO {
     }
 
     override fun getLeftShooterVel(): Double {
-        return leftShooterEncoder.velocity * (40.0/27.0)
+        return leftShooterMotorEncoder.velocity * (40.0 / 27.0)
     }
 
     override fun getRightShooterVel(): Double {
-        return rightShooterEncoder.velocity * (40.0/27.0)
+        return rightShooterMotorEncoder.velocity * (40.0 / 27.0)
     }
 
     override fun setLeftShooter(percent: Double) {
